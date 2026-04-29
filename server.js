ae7359400a20b1e9e7e0b90546738610b77baec3
sawaisinghbusiness <sawaisinghbusiness@gmail.com>
2026-04-29 08:49:03 +0530

    Fix PORT for Railway deployment

--- server.js
+++ server.js
@@ -37,4 +37,5 @@ app.post("/order", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
